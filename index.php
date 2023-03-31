8dc99be6fba296a65a6d80e57f890e913c970bab
EduardoAFR <eduardoafonso.rr@gmail.com>
2023-03-31 11:17:56 -0300

    configurando recuperacao de senha

--- index.php
+++ index.php
@@ -154,6 +154,25 @@ $app->post("/admin/users/:iduser",function($iduser){
 
 });
 
+$app->get("/admin/forgot",function(){
+
+$page = new PageAdmin([
+   "header"=>false,
+   "footer"=>false
+]);
+
+$page->setTpl("forgot"); 
+
+}); 
+
+$app->post("/admin/forgot",function(){
+
+   $_POST["email"]; 
+
+   $user = User::getForgot($_POST["email"]);
+
+});
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -136,6 +136,45 @@ class User extends Model {
 
 	}
 
+	public function getForgot($email){
+
+
+		$sql = new Sql(); 
+
+		$results = $sql->select("
+			SELECT * FROM tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.desemail = :email;
+			",array(
+				":email"=>$email
+			)); 
+	
+		if(count($results) === 0)
+		{
+			throw new \Exception("Nao foi possivel recuperar a senha."); 
+		}
+		else
+		{
+
+			$data = $results[0];
+
+			$results2 = $sql->select("CALL sp_userpasswordsrecoveries_create(:iduser, :desip)",array(
+				":iduser"=>$data["iduser"],
+				":desip"=>$_SERVER["REMOTE_ADDR"]
+			)); 
+
+			if(count($results2) === 0)
+			{
+
+				throw new \Exception ("Não foi possível recuperar a senha");
+
+			}
+			else {
+
+				$dataRecovery = $results2[0]; 
+
+			}
+		}
+	}
+
 }
 
 ?>
